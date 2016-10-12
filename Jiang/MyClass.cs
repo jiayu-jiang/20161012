0fd266379d9b54c51f5e3064901bd60d67d2c6e7
Xamarin Class <xamarinclass@Gjuns-Mac.local>
2016-10-12 13:56:12 +0800

    20161012

---
+++ Jiang/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Jiang
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
---
+++ iOS/MenuViewController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using UIKit;
+
+namespace Jiang.iOS
+{
+	public partial class MenuViewController : UIViewController
+	{
+		public MenuViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+			Title = @"選單";
+		}
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
